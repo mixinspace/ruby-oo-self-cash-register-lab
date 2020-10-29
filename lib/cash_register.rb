f21df85aade8943815e50efcf2a3bf679cfeb801
mixinspace <islammadkaur@gmail.com>
2020-10-29 08:49:36 -0500

    commit tree 829cd73cc9aabfaaae4b9912b4227457448aaf36

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
+    attr_accessor :total, :discount, :title, :price, :quantity
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+    end
 
+    def add_item(title, price)
+        @title = title
+        @price = price
+        @quantity = quantity
+        self.total = total + 0.98
+    end
+end
